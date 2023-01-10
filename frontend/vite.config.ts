10555a1fe84de86b2cb4a6065572efd8f002acf3
s22g357@kagawa-u.ac.jp <s18t316@stu.kagawa-u.ac.jp>
2023-01-10 15:07:56 +0900

    READMEの修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,9 @@
-import { tamaguiExtractPlugin, tamaguiPlugin } from '@tamagui/vite-plugin'    // 追加
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 追加
-const tamaguiCompilerConfig = {
-  components: ['tamagui'],
-  config: 'tamagui.config.ts',
-  useReactNativeWebLite: true,
-}
-
 // https://vitejs.dev/config/
-// https://tamagui.dev/blog/version-one#vite-support
 export default defineConfig({
-  clearScreen: false,
   plugins: [
-    react({
-      fastRefresh: true,
-      jsxPure: true,
-    }),
-    tamaguiPlugin(tamaguiCompilerConfig),
-    tamaguiExtractPlugin(tamaguiCompilerConfig),
+    react()
   ],
 })
